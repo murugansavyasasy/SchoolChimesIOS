5b2abf6fd11050295518468d2c5d04a868098fe2
gayathrigayu8464 <gayathrichitti1330@gmail.com>
2024-12-30 12:13:13 +0530

    translate language variable set added.

--- VoicesnapSchoolApp/LanguageChange/LocalizableStrings.swift
+++ VoicesnapSchoolApp/LanguageChange/LocalizableStrings.swift
@@ -34,7 +34,7 @@ extension String {
         // Retrieve the language code saved in UserDefaults
         
         
-        if let languageCode = defaults.string(forKey: DefaultsKeys.Language),
+        if let languageCode = defaults.string(forKey: DefaultsKeys.languageCode),
            let path = Bundle.main.path(forResource: languageCode, ofType: "lproj"),
            let bundle = Bundle(path: path) {
             
